fb78717ecba470aeb2cbbeb56b5778f63be45460
mahuihui0315 <mhhmhhmhh@outlook.com>
2019-04-09 10:46:22 +0800

    add a pojo of object user

---
+++ SpringMVC/SpringMVC+MyBatis/pojo/User.java
@@ -0,0 +1,22 @@
+package com.springMVC.demo1.pojo;
+
+public class User {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String usename) {
+        this.username = usename;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
